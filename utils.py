1befbf5e98606417d452a6b023b0ff397d09e94a
tjqiulu <tjqiulu@hotmail.com>
2024-10-20 17:06:10 +0800

    updated

---
+++ utils.py
@@ -0,0 +1,9 @@
+from langchain.chains import ConversationChain
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+def get_chat_response(prompt, memory,openai_api_key):
+    model = ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=openai_api_key)
+    chain = ConversationChain(llm=model,memory=memory)
+
+    response = chain.invoke({"input":prompt})
+    return response["response"]
